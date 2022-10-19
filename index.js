7847aefe2395232be926538e3e10db8ad64927d9
Zane Matero <zanematero@Zanes-MacBook-Pro.local>
2022-10-18 19:19:22 -0700

    practicing DOM

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Select some elements...
+// Select page header and style it.
+let header = document.querySelector("#page-header") // 53-55
+header.style.textAlign = 'left' // CSS text-align
+
+//curving text image radius
+let dogImages = document.querySelectorAll(".dog-image")
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = "50px"
+}
+
+//selecting html classes and changing fonts with js
+let dogDescription = document.querySelectorAll(".dog-description")
+for (let i = 0; i < dogDescription.length; i++) {
+    dogDescription[i].style.fontFamily = "Serif"
+}
+
